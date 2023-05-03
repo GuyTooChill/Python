10a19a3e3fae9773e71ba295788f9c4cad583253
Diego G <Diego93gonzo@gmail.com>
2023-05-03 01:57:55 -0400

    updates

---
+++ Week_1/Day_2/user.py
@@ -0,0 +1,26 @@
+class user():
+    def __init__(self, first, last, email, age) -> None:
+        self.first_name = first
+        self.last_name = last
+        self.email = email
+        self.age = age
+        self.is_rewards_member = "False"
+        self.gold_card_points = 0
+
+
+
+def display_info(self):
+    print(self.gold_card_points)
+
+user1 = user
+
+user1.display_info()
+
+
+
+def enroll(self):
+    pass
+
+
+def spend_points(self, amount):
+    pass
