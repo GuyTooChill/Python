0f66f4f534ca61787e0e9957734ad63c40f96b54
Diego G <Diego93gonzo@gmail.com>
2023-05-03 05:55:05 -0400

    updates

---
+++ Week_1/Day_2/bank_account.py
@@ -0,0 +1,12 @@
+class BankAccount:
+    # don't forget to add some default values for these parameters!
+    def __init__(self, int_rate, balance): 
+        pass
+    def deposit(self, amount):
+        pass
+    def withdraw(self, amount):
+        pass
+    def display_account_info(self):
+        pass
+    def yield_interest(self):
+        pass
