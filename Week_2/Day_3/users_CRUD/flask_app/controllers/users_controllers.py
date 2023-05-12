bc53082a7bbd78d71b14770b4d38cfc11f053edb
Diego G <Diego93gonzo@gmail.com>
2023-05-12 07:36:05 -0400

    updates

---
+++ Week_2/Day_3/users_CRUD/flask_app/controllers/users_controllers.py
@@ -0,0 +1,57 @@
+from flask_app import app
+from flask import render_template,redirect,request
+from flask_app.models.users import User
+
+
+# -----------------------------------------------------------
+@app.route('/')         
+def read_users():
+    all_users = User.show_users()
+    return render_template ("read.html", all_users = all_users)
+
+
+
+@app.route('/create')         
+def create_users():
+    return render_template ("create.html")
+
+@app.route('/submit', methods = ['post'])         
+def submit_users():
+    User.create_user(request.form)
+    return redirect ("/")
+
+
+
+@app.route('/show/<int:id>')         
+def show_users(id):
+    user_data = {'id' : id}
+    one_user = User.one_user(user_data)
+    return render_template ("show.html", one_user = one_user)
+
+
+
+@app.route('/edit/<int:id>')         
+def edit_users(id):
+    user_data = {'id' : id}
+    one_user = User.one_user(user_data)
+    return render_template ("edit.html", one_user = one_user)
+
+@app.route('/change/<int:id>', methods = ['post'])         
+def change_users(id):
+    user_data = {
+        "id" : id,
+        "first_name" : request.form['first_name'],
+        "last_name" : request.form['last_name'],
+        "email" : request.form['email']
+    }
+    User.change_user(user_data)
+    return redirect (f"/show/{id}" )
+
+
+
+@app.route('/delete/<int:id>')         
+def delete_users(id):
+    user_data = {'id' : id}
+    User.delete_user(user_data)
+    return redirect ("/")
+
